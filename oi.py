8cae130c37fa0d7eb42d249c97bb6d85982ce8b1
GΔZ42 <97912586+GAZI4@users.noreply.github.com>
2022-01-17 22:11:49 +0300

    Update oi.py

--- oi.py
+++ oi.py
@@ -1,14 +1,8 @@
-a = ord('а')
-ru1 = [chr(i) for i in range(2000)]
-ru1 = str("ru1")
-test = int(input('Enter the shift: '))
-word = input("Enter the message: ").upper()
+test = (int(input('Enter the shift: ')))
+word = (input('Enter the message: '))
+rus = [chr(i) for i in range(2000)]
+work = [rus[(i + test) % 2000] for i in range(2000)]
 end = ''
 for n in word:
-    place = ru1.find(n)
-    places = place + test
-    if n in ru1:
-        end += ru1[places]
-    else:
-        end += n
+	end += work[rus.index(n)]
 print(end)
